0f5c92451a0105db949a369e4d7ac1cd26ad7f8a
Futen Wang <futen.wang@hmsinc.com>
2024-03-29 01:06:04 -0400

    finish

--- development/src/App.js
+++ development/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-import { useEffect, useState } from "react";
-import bakeryData from "./assets/bakery-data.json";
+import { useId, useEffect, useState } from "react";
 import teddy from "./assets/images/jeff.jpeg";
 import teddy2 from "./assets/images/teddy2.jpeg";
 import teddy_wed from "./assets/images/teddy_wed.jpeg";
@@ -16,41 +15,82 @@ import teddy_covid from "./assets/images/teddy_covid.jpeg";
 
 function App() {
 
-  const [data, setData] = useState([teddy, teddy2, teddy_wed, teddy_pilot, teddy_heart, teddy_couch, teddy_sun, teddy_boy, teddy_tourist, teddy_blue, teddy_toys, teddy_covid]);
+  const image_data = [[teddy, 2023], 
+                      [teddy2, 2055], 
+                      [teddy_wed, 1921], 
+                      [teddy_pilot, 1999], 
+                      [teddy_heart, 2019], 
+                      [teddy_couch, 2000], 
+                      [teddy_sun, 1983], 
+                      [teddy_boy, 2016], 
+                      [teddy_tourist, 1900], 
+                      [teddy_blue, 2030], 
+                      [teddy_toys, 2008], 
+                      [teddy_covid, 2001]]
+
+  const [data, setData] = useState(image_data);
   const [cart, setCart] = useState([]);
 
-  const addToFavorites = (item) => {
-    if (!cart.includes(item)) {
-      setCart(prev_cart => [...prev_cart, item])
-        // .filter(price => price < 5))
+  const addToFavorites = (item) => { if (!cart.includes(item)) setCart(prev_cart => [...prev_cart, item]) }
+
+  const sortByDate = () => {
+    let sorted_data = []
+    for (let i = 0; i < data.length + sorted_data.length; i++) {
+      let min = data[0]
+      for (let j = 0; j < data.length; j++) if (data[j][1] < min[1]) min = data[j]
+      if (data.indexOf(min) > -1) data.splice(data.indexOf(min), 1);
+      sorted_data.push(min);
     }
+    setData(sorted_data);
   }
 
-  const removeFromFavorites = (item) => {
-    let new_cart = []
-    for (let i = 0; i < item.length; i++) {
-      if (cart[i] != item) {
-        new_cart.push(cart[i])
-      }
+  function DateFilter(props) {
+    const id = useId();
+    const [input, setInput] = useState(props?.value ?? '');
+    if (props.direction === "Min") {
+      return (
+        <>
+          <label htmlFor={id}>{props.direction} Year: </label>
+          <input id={id} value={input} onInput={e => setInput(e.target.value)}/>
+          <button onClick={(e) => {setData(prev_cart => [...prev_cart].filter(item => item[1] >= input))}}>Filter</button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <label htmlFor={id}>{props.direction} Year: </label>
+          <input id={id} value={input} onInput={e => setInput(e.target.value)}/>
+          <button onClick={(e) => {setData(prev_cart => [...prev_cart].filter(item => item[1] <= input))}}>Filter</button>
+        </>
+      );
     }
-    setCart(prev_cart => new_cart)
-        // .filter(price => price < 5))
   }
 
   const bakeryItemsJSX = data.map((item, index) => ( // TODO: map bakeryData to BakeryItem components
-    <img onClick={(e) => {addToFavorites(item)}} src={item} alt="white teddy bear" width="150" height="100"/> // replace with BakeryItem component
+    <>{item[1]}<img onClick={(e) => {addToFavorites(item)}} src={item[0]} alt="white teddy bear" width="150" height="100"/></> // replace with BakeryItem component
   ))
 
   const cartJSX = cart.length === 0 ? <p>No favorites yet</p>
-  : cart.map((item, index) => <img src={item} alt="white teddy bear" width="150" height="100"/>)
+  : cart.map((item, index) => <img src={item[0]} alt="white teddy bear" width="150" height="100"/>)
 
   return (
     <div className="App">
       <h1>Gallery</h1>
+
+      <DateFilter direction="Min"></DateFilter>
+      <br></br>
+      <DateFilter direction="Max"></DateFilter>
+      <div><button onClick={(e) => {sortByDate()}}>Sort Date</button></div>
+
       {bakeryItemsJSX}
+      <div><button onClick={ () => {setData(image_data)} }>Reset</button></div>
+
       <hr></hr>
+
       <h1>Favorites</h1>
       {cartJSX}
+
+      <div><button onClick={ () => {setCart([])} }>Clear</button></div>
     </div>
   );
 }
